a1674ae1b60e1f6be86cd2ae0bba450962b32c98
Brett Sheffield <brett@gladserv.com>
2020-07-26 16:26:50 +0100

    start/stop server

--- src/server.c
+++ src/server.c
@@ -2,6 +2,10 @@
 /* Copyright (c) 2020 Brett Sheffield <bacs@librecast.net> */
 
 #include <librecast.h>
+#include <signal.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
 #include "config.h"
 #include "log.h"
 #include "server.h"
@@ -10,9 +14,16 @@ lc_ctx_t *lctx;
 lc_socket_t *sock;
 lc_channel_t *chan;
 
+void *server_message_recv(lc_message_t *msg)
+{
+	DEBUG("server received message");
+}
+
 void server_stop()
 {
 	DEBUG("Stopping server");
+	lc_socket_listen_cancel(sock);
+	lc_channel_free(chan);
 	lc_socket_close(sock);
 	lc_ctx_free(lctx);
 }
@@ -22,6 +33,9 @@ int server_start()
 	DEBUG("Starting server");
 	lctx = lc_ctx_new();
 	sock = lc_socket_new(lctx);
-
+	chan = lc_channel_new(lctx, "radio freedom");
+	lc_channel_bind(sock, chan);
+	lc_channel_join(chan);
+	lc_socket_listen(sock, server_message_recv, NULL);
 	return 0;
 }
--- test/0000-0003.c
+++ test/0000-0003.c
@@ -5,53 +5,19 @@
 #include "../src/config.h"
 #include "../src/server.h"
 #include <librecast.h>
-#include <signal.h>
+#include <time.h>
 #include <unistd.h>
-
-int gotmsg = 0;
-
-void callback_recv(lc_message_t *msg)
-{
-	pid_t pid = getpid();
-	fprintf(stdout, "got a msg\n");
-	gotmsg = 1;
-	kill(pid, SIGUSR1); /* interrupt sleep() */
-}
 
 int main()
 {
-	test_name("ping server");
+	struct timespec t = { 0, 0 };
 
+	test_name("start/stop server");
 	config.debug = 1;
 	config.loglevel = 127;
 
-	server_start();
-
-	lc_ctx_t *lctx;
-	lc_socket_t *sock;
-	lc_channel_t *chan;
-	lc_message_t msg;
-	int opt;
-
-	lctx = lc_ctx_new();
-	sock = lc_socket_new(lctx);
-	chan = lc_channel_new(lctx, "hello multicast");
-	test_assert(lc_channel_bind(sock, chan) == 0, "bind channel to socket");
-	test_assert(lc_socket_listen(sock, callback_recv, NULL) == 0, "lc_socket_listen()");
-
-	/* PING */
-	opt = LC_OP_PING;
-	lc_msg_init(&msg);
-	test_assert(lc_msg_set(&msg, LC_ATTR_OPCODE, &opt) == 0, "lc_msg_set()");
-	test_assert(lc_msg_send(chan, &msg) == 0, "lc_msg_send()");
-
-	/* wait for PONG */
-	sleep(2); /* sleep 2s or until SIGUSR1 */
-	test_assert(gotmsg, "ping reply timeout");
-
-	lc_channel_free(chan);
-	lc_socket_close(sock);
-	lc_ctx_free(lctx);
+	test_assert(server_start() == 0, "server_start()");
+	nanosleep(&t, &t); /* yield */
 	server_stop();
 
 	return fails;
