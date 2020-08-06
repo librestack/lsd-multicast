9a1c24a445a2c43a40aa8211cd3129504807a2a4
Brett Sheffield <brett@gladserv.com>
2020-08-06 23:21:38 +0100

    add server loop

--- src/server.c
+++ src/server.c
@@ -1,7 +1,9 @@
 /* SPDX-License-Identifier: GPL-3.0-or-later */
 /* Copyright (c) 2020 Brett Sheffield <bacs@librecast.net> */
 
+#include <errno.h>
 #include <librecast.h>
+#include <signal.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
@@ -9,34 +11,47 @@
 #include "log.h"
 #include "server.h"
 
+static volatile sig_atomic_t running = 1;
 lc_ctx_t *lctx;
 lc_socket_t *sock;
 lc_channel_t *chan;
 
-void server_message_recv(lc_message_t *msg)
+void sighandler(int sig)
 {
-	DEBUG("server received message");
+	running = 0;
 }
 
-void server_stop()
+void server_stop(void)
 {
 	DEBUG("Stopping server");
-	lc_socket_listen_cancel(sock);
-	lc_channel_free(chan);
-	lc_socket_close(sock);
-	lc_ctx_free(lctx);
+	kill(getpid(), SIGINT);
 }
 
-int server_start()
+int server_start(void)
 {
 	DEBUG("Starting server");
 	char dbpath[] = "/tmp/lsdbd.tmp.XXXXXX";
+	ssize_t byt_recv;
+	struct sigaction sa = { .sa_handler = sighandler };
+
+	sigemptyset(&sa.sa_mask);
+	sigaction(SIGINT, &sa, NULL);
+
+	lc_message_t msg;
 	lctx = lc_ctx_new();
 	lc_db_open(lctx, mkdtemp(dbpath));
 	sock = lc_socket_new(lctx);
-	chan = lc_channel_new(lctx, "radio freedom"); // FIXME: channel from config
+	chan = lc_channel_new(lctx, "auth"); // FIXME: channel from config
 	lc_channel_bind(sock, chan);
 	lc_channel_join(chan);
-	lc_socket_listen(sock, server_message_recv, NULL);
+	while (running) {
+		byt_recv = lc_msg_recv(sock, &msg);
+		if (byt_recv == -1 && errno == EINTR) continue;
+		lc_msg_free(&msg);
+	}
+	lc_channel_free(chan);
+	lc_socket_close(sock);
+	lc_ctx_free(lctx);
+
 	return 0;
 }
--- test/0000-0003.c
+++ test/0000-0003.c
@@ -5,20 +5,34 @@
 #include "../src/config.h"
 #include "../src/server.h"
 #include <librecast.h>
+#include <pthread.h>
 #include <time.h>
 #include <unistd.h>
 
+void *stopthread(void *arg)
+{
+	struct timespec t = { 0, 1 };
+	nanosleep(&t, &t);
+	server_stop();
+	pthread_exit(arg);
+}
+
 int main()
 {
-	struct timespec t = { 0, 0 };
+	pthread_t thread;
 
 	test_name("start/stop server");
 	config.debug = 1;
 	config.loglevel = 127;
 
+	/* create thread to stop server */
+	pthread_attr_t attr = {};
+	pthread_attr_init(&attr);
+	pthread_create(&thread, &attr, stopthread, NULL);
+
 	test_assert(server_start() == 0, "server_start()");
-	nanosleep(&t, &t); /* yield */
-	server_stop();
+
+	pthread_join(thread, NULL);
 
 	return fails;
 }
