136b4b2872b50bf90134ba9a913140ff7211bf50
Brett Sheffield <brett@gladserv.com>
2020-08-08 10:58:02 +0100

    refactor auth_pack()

--- src/auth.c
+++ src/auth.c
@@ -2,11 +2,25 @@
 #include <stdlib.h>
 #include <string.h>
 
+void * auth_pack_field(struct iovec *iov, void *ptr)
+{
+	if (!iov)
+		memset(ptr++, 0, 1);
+	else {
+		memcpy(ptr++, &iov->iov_len, 1);
+		memcpy(ptr, iov->iov_base, iov->iov_len);
+		ptr += iov->iov_len;
+	}
+	return ptr;
+}
+
 ssize_t auth_pack(struct iovec *data, struct iovec *repl,
 		  struct iovec *user, struct iovec *mail,
 		  struct iovec *pass, struct iovec *serv)
 {
 	void * ptr;
+	struct iovec *iovs[] = { repl, user, mail, pass, serv };
+	const int iov_count = sizeof iovs / sizeof iovs[0];
 	if (!data) {
 		errno = EINVAL;
 		return -1;
@@ -15,54 +29,19 @@ ssize_t auth_pack(struct iovec *data, struct iovec *repl,
 		errno = EINVAL;
 		return -1;
 	}
-	if ((repl && repl->iov_len > UCHAR_MAX) || (user && user->iov_len > UCHAR_MAX)
-	 || (mail && mail->iov_len > UCHAR_MAX) || (pass && pass->iov_len > UCHAR_MAX)
-	 || (serv && serv->iov_len > UCHAR_MAX))
-	{
-		errno = E2BIG;
-		return -1;
+	for (int i = 0; i < iov_count; i++) {
+		if (iovs[i] && iovs[i]->iov_len > UCHAR_MAX) {
+			errno = E2BIG;
+			return -1;
+		}
 	}
-	data->iov_len = 5; /* 5 x char for lengths */
-	if (repl) data->iov_len += repl->iov_len;
-	if (user) data->iov_len += user->iov_len;
-	if (mail) data->iov_len += mail->iov_len;
-	if (pass) data->iov_len += pass->iov_len;
-	if (serv) data->iov_len += serv->iov_len;
-	data->iov_base = malloc(data->iov_len);
-	ptr = data->iov_base;
-	if (!repl)
-		memset(ptr++, 0, 1);
-	else {
-		memcpy(ptr++, &repl->iov_len, 1);
-		memcpy(ptr, repl->iov_base, repl->iov_len);
-		ptr += repl->iov_len;
+	data->iov_len = iov_count; /* n char for lengths */
+	for (int i = 0; i < iov_count; i++) {
+		if (iovs[i]) data->iov_len += iovs[i]->iov_len;
 	}
-	if (!user)
-		memset(ptr++, 0, 1);
-	else {
-		memcpy(ptr++, &user->iov_len, 1);
-		memcpy(ptr, user->iov_base, user->iov_len);
-		ptr += user->iov_len;
-	}
-	if (!mail)
-		memset(ptr++, 0, 1);
-	else {
-		memcpy(ptr++, &mail->iov_len, 1);
-		memcpy(ptr, mail->iov_base, mail->iov_len);
-		ptr += mail->iov_len;
-	}
-	if (!pass)
-		memset(ptr++, 0, 1);
-	else {
-		memcpy(ptr++, &pass->iov_len, 1);
-		memcpy(ptr, pass->iov_base, pass->iov_len);
-		ptr += pass->iov_len;
-	}
-	if (!serv)
-		memset(ptr++, 0, 1);
-	else {
-		memcpy(ptr++, &serv->iov_len, 1);
-		memcpy(ptr, serv->iov_base, serv->iov_len);
+	ptr = data->iov_base = malloc(data->iov_len);
+	for (int i = 0; i < iov_count; i++) {
+		ptr = auth_pack_field(iovs[i], ptr);
 	}
 	return data->iov_len;
 }
