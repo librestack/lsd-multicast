4a7682a1de7358770774c3c85ba8711b4281b230
Brett Sheffield <brett@gladserv.com>
2020-08-11 20:05:10 +0100

    echo module working

--- modules/echo.c
+++ modules/echo.c
@@ -20,13 +20,21 @@ void finit(void)
 void handle_msg(lc_message_t *msg)
 {
 	TRACE("echo.so %s()", __func__);
-	DEBUG("I have received the message");
+
+	lc_ctx_t *lctx = lc_channel_ctx(msg->chan);
+	lc_socket_t *sock = lc_channel_socket(msg->chan);
+	lc_channel_t *chan_repl = lc_channel_new(lctx, "repl");
+	lc_channel_bind(sock, chan_repl);
+	lc_msg_send(chan_repl, msg);
 
 	/* TODO:
 	 * - decrypt
 	 * - call opcode handler
 	 * - unpack
 	 */
+
+	lc_channel_unbind(chan_repl);
+
 	DEBUG("message says '%.*s'", (int)msg->len, (char *)msg->data);
 }
 
--- test/0000-0008.c
+++ test/0000-0008.c
@@ -11,22 +11,27 @@
 void *testthread(void *arg)
 {
 	lc_ctx_t *lctx;
-	lc_socket_t *sock;
-	lc_channel_t *chan;
-	lc_message_t msg;
+	lc_socket_t *sock, *sock_repl;
+	lc_channel_t *chan, *chan_repl;
+	lc_message_t msg, msg_repl;
 	char data[] = "hello";
 	int opt = 1;
 
 	lctx = lc_ctx_new();
 	sock = lc_socket_new(lctx);
+	sock_repl = lc_socket_new(lctx);
 	lc_socket_setopt(sock, IPV6_MULTICAST_LOOP, &opt, sizeof(opt));
 	chan = lc_channel_new(lctx, config.handlers->channel);
+	chan_repl = lc_channel_new(lctx, "repl");
 	lc_channel_bind(sock, chan);
+	lc_channel_bind(sock_repl, chan_repl);
+	lc_channel_join(chan_repl);
 
 	lc_msg_init_data(&msg, &data, strlen(data), NULL, NULL);
-	test_sleep(0, 999999); /* give server a chance to be ready */
 	lc_msg_send(chan, &msg);
-	test_sleep(0, 999999); /* give server a chance to read message */
+	lc_msg_recv(sock, &msg_repl);
+	test_expectn(data, msg_repl.data, msg_repl.len);
+	lc_msg_free(&msg_repl);
 
 	lc_ctx_free(lctx);
 	server_stop();
